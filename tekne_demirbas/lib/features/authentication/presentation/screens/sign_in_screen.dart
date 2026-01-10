8eb6b2ef9670a84898a30784cd8d6a09069322e5
Blu3LordS00n <emir.ozcelik196@gmail.com>
2026-01-10 19:36:43 +0300

    Sign In screen functionality & frontend

--- tekne_demirbas/lib/features/authentication/presentation/screens/sign_in_screen.dart
+++ tekne_demirbas/lib/features/authentication/presentation/screens/sign_in_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tekne_demirbas/features/authentication/presentation/widgets/common_text_field.dart';
+import 'package:tekne_demirbas/routes/routes.dart';
 import 'package:tekne_demirbas/utils/appstyles.dart';
 import 'package:tekne_demirbas/utils/size_config.dart';
 
@@ -10,13 +14,130 @@ class SignInScreen extends ConsumerStatefulWidget {
 }
 
 class _SignInScreenState extends ConsumerState<SignInScreen> {
+  final _emailEditingController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  void dispose() {
+    _emailEditingController.dispose();
+    _passwordController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     SizeConfig.init(context);
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [Text('Giris yap', style: Appstyles.titleTextStyle)],
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(
+            SizeConfig.getProportionateWidth(10),
+            SizeConfig.getProportionateHeight(50),
+            SizeConfig.getProportionateWidth(10),
+            0,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('Giris yap', style: Appstyles.titleTextStyle),
+                SizedBox(height: SizeConfig.getProportionateHeight(25)),
+                CommonTextField(
+                  hintText: 'Mail adresi',
+                  textInputType: TextInputType.emailAddress,
+                  obscureText: false,
+                  controller: _emailEditingController,
+                ),
+                SizedBox(height: SizeConfig.getProportionateHeight(10)),
+                CommonTextField(
+                  hintText: 'Sifre',
+                  textInputType: TextInputType.text,
+                  obscureText: true,
+                  controller: _passwordController,
+                ),
+                SizedBox(height: SizeConfig.getProportionateHeight(25)),
+
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: SizeConfig.getProportionateHeight(50),
+                    width: SizeConfig.screenWidth,
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 43, 253, 2),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Text(
+                      'Giris yap!',
+                      style: Appstyles.normalTextStyle.copyWith(
+                        color: Colors.white,
+                        fontSize: SizeConfig.getProportionateHeight(20),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: SizeConfig.getProportionateHeight(10)),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: SizeConfig.getProportionateHeight(1),
+                      width: SizeConfig.screenWidth * 0.4,
+                      decoration: BoxDecoration(color: Colors.grey),
+                    ),
+                    SizedBox(height: SizeConfig.getProportionateWidth(5)),
+                    Text('OR', style: Appstyles.normalTextStyle),
+                    Container(
+                      height: SizeConfig.getProportionateWidth(1),
+                      width: SizeConfig.screenWidth * 0.4,
+                      decoration: BoxDecoration(color: Colors.grey),
+                    ),
+                  ],
+                ),
+                SizedBox(height: SizeConfig.getProportionateHeight(10)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: SizeConfig.getProportionateHeight(40),
+                      width: SizeConfig.screenWidth * 0.75,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: Colors.black,
+                          style: BorderStyle.solid,
+                          width: 2,
+                        ),
+                      ),
+                      child: const FaIcon(
+                        FontAwesomeIcons.google,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: SizeConfig.getProportionateHeight(40)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Hesabın yok mu?', style: Appstyles.normalTextStyle),
+                    GestureDetector(
+                      onTap: () {
+                        context.goNamed(AppRoutes.register.name);
+                      },
+                      child: Text(
+                        ' Kayıt ol!',
+                        style: Appstyles.normalTextStyle.copyWith(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
---
+++ tekne_demirbas/lib/features/authentication/presentation/widgets/common_text_field.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tekne_demirbas/utils/appstyles.dart';
+
+class CommonTextField extends StatelessWidget {
+  const CommonTextField({
+    super.key,
+    required this.hintText,
+    required this.textInputType,
+    required this.obscureText,
+    required this.controller,
+  });
+
+  final String hintText;
+  final TextInputType textInputType;
+  final bool? obscureText;
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: textInputType,
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: Appstyles.normalTextStyle,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: const BorderSide(color: Colors.grey, width: 1.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: const BorderSide(color: Colors.grey, width: 1.0),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: const BorderSide(color: Colors.grey, width: 1.0),
+        ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+      ),
+    );
+  }
+}
